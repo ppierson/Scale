1d0a7e858cc4d2ca5ccf997af5349d11c53f4121
unknown <patrick.d.pierson@gmail.com>
2023-11-04 13:35:26 -0400

    Added ground terrain tileset, floor, and tweaked character movement to enable jumping and smoother movement.

--- Scenes/Player/Player.cs
+++ Scenes/Player/Player.cs
@@ -4,11 +4,11 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	[Export]
-	public int MovementSpeed { get; set; } = 350;
+	public int MovementSpeed { get; set; } = 250;
 	[Export]
-	public int GravityVelocity { get; set; } = 200;
+	public int GravityVelocity { get; set; } = 600;
 	[Export]
-	public int jumpVelocity { get; set; } = 300;
+	public int jumpVelocity { get; set; } = 500;
 
 	private AnimatedSprite2D AnimatedSprite2D = null; 
 
@@ -24,11 +24,14 @@ public partial class Player : CharacterBody2D
 		var velocity = inputVelocity(delta);
 		velocity = addGravity(velocity, delta);	// Player movement vector
 
-		this.Velocity = velocity;
+		GD.Print($"Velocity: X - {velocity.X}, Y - {velocity.Y}");
+
 		updateAnimation(velocity);
 
-		// MoveAndSlide();
-    }
+		velocity.Y += this.Velocity.Y;
+		this.Velocity = velocity;
+		MoveAndSlide();
+	}
 
 	private Vector2 inputVelocity(double delta) 
 	{
@@ -47,10 +50,10 @@ public partial class Player : CharacterBody2D
 
 		if (Input.IsActionJustPressed("jump") && IsOnFloor()) 
 		{
-			velocity.Y -= jumpVelocity * (float)delta;
+			velocity.Y = -jumpVelocity;
 		}
 
-		velocity.X = velocity.X * (MovementSpeed * (float)delta);
+		velocity.X = velocity.X * MovementSpeed;
 
 		return velocity;
 	}
@@ -68,12 +71,11 @@ public partial class Player : CharacterBody2D
 			// Jump animation
 			AnimatedSprite2D.Animation = "jump";
 		}
-		else if (velocity.Y > 0) {
+		else if (velocity.Y > 0 && !IsOnFloor()) {
 			// Fall animation
 			AnimatedSprite2D.Animation = "fall";
 		}
-		else if (velocity.X != 0)
-		{
+		else if (velocity.X != 0) {
 			// Run animation
 			AnimatedSprite2D.Animation = "run";
 			AnimatedSprite2D.Play();
@@ -85,7 +87,9 @@ public partial class Player : CharacterBody2D
 		}
 
 		// Flip direction for X velocity
-		AnimatedSprite2D.FlipH = velocity.X < 0;
+		if (velocity.X != 0) {
+			AnimatedSprite2D.FlipH = velocity.X < 0;
+		}
 	}
 
 	private void updatePosition(Vector2 velocity, double delta) 
